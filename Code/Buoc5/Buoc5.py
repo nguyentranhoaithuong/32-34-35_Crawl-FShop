2725399b173b7bb34855b385adebb4bfbc9a2285
Khánh Vy <125074740+KhanhVy-47K212@users.noreply.github.com>
2023-12-05 19:11:09 +0700

    Update Buoc5.py

--- Code/Buoc5/Buoc5.py
+++ Code/Buoc5/Buoc5.py
@@ -3,11 +3,12 @@ import re
 from underthesea import word_tokenize
 from emot.emo_unicode import UNICODE_EMOJI, EMOTICONS_EMO
 from underthesea import word_tokenize
+
 # Đường dẫn đến thư mục chứa các bình luận
-root_dir = 'E:\Code\BTL_KPW\Cmt_raw'
+root_dir = 'D:\Khai pha web\CK\Cmt_raw'
 
 # Đường dẫn đến thư mục TXLCMT
-txl_dir = 'E:\Code\BTL_KPW\TXLCMT'
+txl_dir = 'D:\Khai pha web\CK\TXLCMT'
 
 # Kiểm tra xem thư mục TXLCMT có tồn tại hay không
 if not os.path.exists(txl_dir):
@@ -47,20 +48,18 @@ for product_dir in os.listdir(root_dir):
                                 text = text.replace(x, "_".join(UNICODE_EMOJI[x].replace(",","").replace(":","").split()))
                                     
                             return text
-
                         text_pre=text.replace("\n","")  # Remove the newline command
                         text_pre=text.lower() # Convert text to lowercase
-                        text_pre=re.sub(r'[^\w\s]','',text_pre) # Remove punctuation
+                        text_pre = word_tokenize(text, format="text")
+                        text_pre = re.sub(r'[^\w\s_]','',text_pre)
                         text_pre = re.sub("\d+", " ", text_pre) # Remove number
                         text_pre = re.sub(r"!@#$\[\']", "", text_pre) # Remove character: !@#$
-                        text_pre= word_tokenize(text, format="text")
-                        filename=os.path.join('E:\Code\KPW',"vietnamese-stopwords.txt")
+                        filename=os.path.join('D:/Khai pha web/CK',"vietnamese-stopwords.txt")
                         with open(filename,'r',encoding='utf-8') as f:
                             List_StopWords=f.read().split("\n")
-                        #remove stop words
+                                                #remove stop words
                         text_pre=" ".join(text for text in text_pre.split() if text not in List_StopWords)
-                        text_pre=text_pre.replace(",","").replace(".","").replace("  "," ").replace("!","").replace("...","")
-                        
+                        # text_pre= word_tokenize(text, format="text")                        
                         all_files_content += str(text_pre) + '\n'  # Thêm nội dung của file vào chuỗi
 
             output_filepath = os.path.join(new_product_path, sentiment_dir + '.txt')
