f1119d796d8fe5b79d80874790c5a56e5a14773e
ttrinh1312 <117828617+ttrinh1312@users.noreply.github.com>
2023-12-07 16:03:30 +0700

    Update B1_LinkSP.py

--- Code/Buoc1/B1_LinkSP.py
+++ Code/Buoc1/B1_LinkSP.py
@@ -4,21 +4,21 @@ from selenium.common.exceptions import NoSuchElementException
 import os
 
 driver = webdriver.Chrome()
-all_article_links = set()
+All_LinksBao = set()
 
 driver.get(f'https://fptshop.com.vn/may-tinh-xach-tay?sort=ban-chay-nhat&trang=15')
-article_links = driver.find_elements(By.CSS_SELECTOR, 'div.cdt-product__info')
-for link in article_links:
+LinksBao = driver.find_elements(By.CSS_SELECTOR, 'div.cdt-product__info')
+for link in LinksBao:
     try:
         links = link.find_elements(By.TAG_NAME, 'a')
         for a in links:
             href = a.get_attribute('href')
             if "https://fptshop.com.vn/so-sanh-san-pham?" not in href:
-                all_article_links.add(href)
+                All_LinksBao.add(href)
     except NoSuchElementException:
         pass
 
-filename = os.path.join("D:\KPW\BTL", "Link.txt")
+filename = os.path.join("D:\Khai_pha_web\BaoCao", "B1_Link.txt")
 with open(filename, 'w', encoding='utf-8') as file:
-    for link in all_article_links:
+    for link in All_LinksBao:
         file.write(link + '\n')
