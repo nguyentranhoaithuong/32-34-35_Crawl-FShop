369b93e3248ec58c5285ae71741ed4c5bf2f4dcd
Nguyễn Trần Hoài Thương <125058347+nguyentranhoaithuong@users.noreply.github.com>
2023-12-09 20:52:30 +0700

    Update buoc7.py

--- Code/Buoc7/buoc7.py
+++ Code/Buoc7/buoc7.py
@@ -16,46 +16,50 @@ thumucgoc = 'E:\Code\BTL_KPW\TXLCMT'
 for folder in os.listdir(thumucgoc):
     thumuccon = os.path.join(thumucgoc, folder)
     if os.path.isdir(thumuccon):
-        print(f"Xử lý thư mục: {thumuccon}")
-        for file in os.listdir(thumuccon):
-            file_path = os.path.join(thumuccon, file)
-            if file.endswith('.txt'):
-                if file=='Pos.txt':
-                    print("BÌNH LUẬN TÍCH CỰC CỦA ",f" {thumuccon}")
-                elif file=='Nes.txt':
-                    print("BÌNH LUẬN TIÊU CỰC CỦA ",f" {thumuccon}")
-                if file=='neutral.txt':
-                    print("BÌNH LUẬN TRUNG LẬP CỦA ",f" {thumuccon}")
-                TachTu=[]
-                with open(file_path, 'r', encoding='utf-8') as f:
-                    lines = f.read()
+        print("------------------------------------------------------------------------------------------------------------------")
+        print(f"Xử lý dòng sản phẩm: {thumuccon}")
+        for f in os.listdir(thumuccon):
+            tmc=os.path.join(thumuccon,f)
+            if os.path.isdir(tmc):
+                print(f"Xử lý sản phẩm: {tmc}")
+                for file in os.listdir(tmc):
+                    file_path=os.path.join(tmc,file)
+                    if file.endswith(".txt"):
+                        if file=='Positive.txt':
+                            print("----BÌNH LUẬN TÍCH CỰC CỦA ",f" {tmc}")
+                        elif file=='Negative.txt':
+                            print("----BÌNH LUẬN TIÊU CỰC CỦA ",f" {tmc}")
+                        if file=='Neutral.txt':
+                            print("----BÌNH LUẬN TRUNG LẬP CỦA ",f" {tmc}")
+                        TachTu=[]
+                        with open(file_path,"r",encoding="utf-8") as f:
+                            lines = f.read()
+                            TachTu+=lines.split()
+                            # print(TachTu)
+                            if TachTu!=[]:
+                                print("Number of words: ", len(TachTu))
+                                
+                                frequency_dist = FreqDist(word.lower() for word in TachTu)
 
-                        # Tách từ
-                    TachTu+=lines.split()
-                    
-                    if TachTu!=[]:
-                        print("Number of words: ", len(TachTu))
-                        
-                        frequency_dist = FreqDist(word.lower() for word in TachTu)
+                                # In 50 từ xuất hiện nhiều nhất
+                                print("Top 50 words:")
+                                print(frequency_dist.most_common(50))
 
-                        # In 50 từ xuất hiện nhiều nhất
-                        print("Top 50 words:")
-                        print(frequency_dist.most_common(50))
+                                # Vẽ biểu đồ tần suất xuất hiện của 20 từ phổ biến nhất
+                                plt.figure(figsize=(12, 6))
+                                frequency_dist.plot(20, cumulative=False)
+                                
+                                # Tạo word cloud từ frequency distribution
+                                wordcloud = WordCloud(width=800, height=400, background_color='white').generate_from_frequencies(frequency_dist)
 
-                        # Vẽ biểu đồ tần suất xuất hiện của 20 từ phổ biến nhất
-                        plt.figure(figsize=(12, 6))
-                        frequency_dist.plot(20, cumulative=False)
-                        
-                        # Tạo word cloud từ frequency distribution
-                        wordcloud = WordCloud(width=800, height=400, background_color='white').generate_from_frequencies(frequency_dist)
+                                # Hiển thị word cloud
+                                plt.figure(figsize=(10, 5))
+                                plt.imshow(wordcloud, interpolation='bilinear')
+                                plt.axis("off")
+                                plt.title('Word Cloud')
+                                plt.show()
+                                print("\n ")
+                            else:
+                                print(f"File {file_path} trống. Bỏ qua...")
+                                print("\n")
 
-                        # Hiển thị word cloud
-                        plt.figure(figsize=(10, 5))
-                        plt.imshow(wordcloud, interpolation='bilinear')
-                        plt.axis("off")
-                        plt.title('Word Cloud')
-                        plt.show()
-                        print("\n ")
-                    else:
-                        print(f"File {file_path} trống. Bỏ qua...")
-                        print("\n")
